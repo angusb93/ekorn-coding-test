e50829f3f4f9174c9ad423f20eae541c83ffec8f
Angus Buick <angusbuick@gmail.com>
2025-05-07 17:16:47 +0100

    refactor functions to utils

--- src/App.svelte
+++ src/App.svelte
@@ -4,43 +4,9 @@
   import type { StudentDataItem } from './lib/data';
   import type { Scores } from './lib/data';
   import type { Student } from './lib/data';
+  import { calculateAverageScore, calculateAge } from './lib/utils';
 
   function transformStudentData(studentsData: StudentDataItem[]): Student[] {
-    const calculateAge = (birthdateString: string): number => {
-      const birthdate = new Date(birthdateString);
-
-      // Handle if date string doesnt parse. return 0.
-      // I would like to be able to return null here or throw an error.
-      if (isNaN(birthdate.getTime())) {
-        console.error(`Couldn't parse birthdate: "${birthdateString}"`);
-        return 0;
-      }
-
-      const today = new Date();
-      let age = today.getFullYear() - birthdate.getFullYear();
-      const monthDifference = today.getMonth() - birthdate.getMonth();
-
-      if (
-        monthDifference < 0 ||
-        (monthDifference === 0 && today.getDate() < birthdate.getDate())
-      ) {
-        age--;
-      }
-
-      return age;
-    };
-
-    const calculateAverageScore = (scores: Scores): number => {
-      const numbers = Object.values(scores).filter(Number.isFinite);
-      if (numbers.length === 0) {
-        console.error(`Cannot parse scores`);
-        return 0;
-      }
-
-      const total = numbers.reduce((sum, n) => sum + n, 0);
-      return Math.round(total / numbers.length);
-    };
-
     const transformedStudentsData = studentsData.map(student => {
       return {
         id: String(student.id),
